5554722891720076d231aee63ba675e8a7b60e77
vncntcltt <vncntcltt@users.noreply.github.com>
2018-11-21 17:50:29 +0100

    update component

--- src/components/CountrySelectFilter.js
+++ src/components/CountrySelectFilter.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { func, array, bool, string } from 'prop-types';
 
 class CountrySelectFilter extends React.Component {
+
+  static propTypes = {    
+    values: array.isRequired,    
+    onFilterChange: func.isRequired,
+    label: string,
+    addAll: bool
+  }
+
+  static defaultProps = {
+    label: '',
+    addAll: false
+  }
 
   onFilterChange = e => {
     if (this.props.onFilterChange) {
@@ -11,7 +24,7 @@ class CountrySelectFilter extends React.Component {
   render() {
     return (
     <span>
-      <label>{this.props.label} </label>
+      <label>{this.props.label}</label>
       <select onChange={this.onFilterChange}>
       {this.props.addAll ? <option value="">All</option> : ''}
       {this.props.values.map(val => 
