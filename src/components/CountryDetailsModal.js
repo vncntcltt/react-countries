94c5bde539e71406be6ed2788d99c83f4f50120e
vncntcltt <vncntcltt@users.noreply.github.com>
2018-12-03 17:48:55 +0100

    Use render props

--- src/components/CountryDetailsModal.js
+++ src/components/CountryDetailsModal.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import Modal from 'react-bootstrap/lib/Modal';
 import Button from 'react-bootstrap/lib/Button';
-import { withNamespaces } from 'react-i18next';
+import { NamespacesConsumer } from 'react-i18next';
 
 import CountryCard from './CountryCard';
 
 class CountryDetailsModal extends React.Component {
 
   render() {
-    const { t, country, onHide, tReady, i18nOptions, defaultNS, reportNS,  ...rest } = this.props;
+    const { country, onHide } = this.props;
     return (
-      <Modal
-        {...rest}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">{t('dialog.title.details')}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {country && <CountryCard country={country} />}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={onHide}>{t('dialog.button.close')}</Button>
-        </Modal.Footer>
-      </Modal>
+      <NamespacesConsumer>
+        {
+          t => (
+            <Modal
+              {...this.props}
+              size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">{t('dialog.title.details')}</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                {country && <CountryCard country={country} />}
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={onHide}>{t('dialog.button.close')}</Button>
+              </Modal.Footer>
+            </Modal>
+          )
+        }
+      </NamespacesConsumer>
     );
   }
   
 }
 
-export default withNamespaces()(CountryDetailsModal);
+export default CountryDetailsModal;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,30 @@ import React from 'react';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import Nav from 'react-bootstrap/lib/Nav';
 import LinkContainer from 'react-router-bootstrap/lib/LinkContainer';
-import { withNamespaces } from 'react-i18next';
+import { NamespacesConsumer } from 'react-i18next';
 
-const MainNavbar = ({ t }) => {
+const MainNavbar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-      <LinkContainer to="/countries">      
-        <Navbar.Brand>{t('countries.title')}</Navbar.Brand>
-      </LinkContainer>
-      <Nav className="mr-auto">
-        <LinkContainer to="/settings">   
-          <Nav.Link>{t('settings.title')}</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/about">
-          <Nav.Link>{t('about.title')}</Nav.Link>
-        </LinkContainer>
-      </Nav>    
-    </Navbar>
+    <NamespacesConsumer>
+      {
+        t => (
+          <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+            <LinkContainer to="/countries">      
+              <Navbar.Brand>{t('countries.title')}</Navbar.Brand>
+            </LinkContainer>
+            <Nav className="mr-auto">
+              <LinkContainer to="/settings">   
+                <Nav.Link>{t('settings.title')}</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <Nav.Link>{t('about.title')}</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar>
+        )
+    }
+    </NamespacesConsumer>
   );
 };
 
-export default withNamespaces()(MainNavbar);
+export default MainNavbar;
