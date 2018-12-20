4b4c09d42461842832ec0bb42d1746ff05ed4819
vncntcltt <vncntcltt@users.noreply.github.com>
2018-12-20 17:48:29 +0100

    Add service worker to cache content for offline use

--- src/index.js
+++ src/index.js
@@ -2,17 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
+import { ToastContainer, toast } from 'react-toastify'
 
-import './i18n'
+import i18n from './i18n'
 import { store, history } from './store'
 import App from './components/App'
 import './index.css'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App />
+      <>
+        <App />
+        <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} />
+      </>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register({
+  onUpdate: () => {
+    toast.warn(i18n.t('serviceWorker.msg.cacheUpdate'))
+  },
+  onSuccess: () => {
+    toast.info(i18n.t('serviceWorker.msg.cacheSuccess'))
+  }
+})
