03dec916085acf5b9453d4ce1f85ce28a7013fa7
vncntcltt <vncntcltt@users.noreply.github.com>
2019-01-07 12:05:18 +0100

    set min zoom to 2

--- src/components/country/CountryMap.js
+++ src/components/country/CountryMap.js
@@ -20,7 +20,7 @@ class CountryMap extends React.Component {
   }
 
   componentDidMount() {
-    this.map = L.map('map').setView([0, 0], 2)
+    this.map = L.map('map', { minZoom: 2 }).setView([0, 0], 2)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map)
